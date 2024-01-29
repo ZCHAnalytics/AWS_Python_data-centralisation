3bcec0d0217721cde905fdc398b9f568cdc18437
Zulfia <zchanalytics@gmail.com>
2024-01-29 16:32:29 +0000

    "Removes draft files"

--- sql_cripts/MS_4_Task_1_stores_by_country.sql
+++
@@ -1,5 +0,0 @@
-/* TASK 1: Query the database to find which countries we currently operate in and which now has the most stores. */
-
-SELECT country_code, COUNT(store_code) AS total_no_stores FROM dim_store_details
-GROUP by country_code 
-ORDER BY total_no_stores DESC;
--- sql_cripts/MS_4_Task_7_staff_by_country.sql
+++
@@ -1,6 +0,0 @@
-/* Task 6: Query database to find the overall staff numbers in each location around the world.  */
-
-SELECT SUM(staff_numbers) AS total_staff_number, country_code
-FROM dim_store_details
-GROUP BY country_code
-ORDER BY total_staff_number DESC;
